d925bdb0604161d6d93b3b09e4ca948fd66ab5b1
Danilo Batista da Silva <dsilva@travix.local>
2020-01-27 19:45:11 +0100

    Adds Project with first version

---
+++ Api/Controllers/ContentController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMostUsedWords.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace MyMostUsedWords.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContentController: Controller
+    {
+        const string sourceLang = "pt";
+        const string targetLang = "en";
+        
+        HttpClient client;
+
+        public ContentController()
+        {
+            client = new HttpClient();
+        }
+
+        [HttpPost]
+        //public async Task<IEnumerable<WordCount>> Post()
+        public async Task<string> Post()
+        {
+            var reader = new StreamReader(HttpContext.Request.Body);
+            var text = await reader.ReadToEndAsync();
+
+            var wordsCountList = new List<WordCount>();
+            var wordsInText = text.Split(' ');
+
+            foreach (var word in wordsInText)
+            {
+                var wordCount = wordsCountList.FirstOrDefault(w => w.Word == word);
+                if (wordCount == null)
+                {
+                    var translation = "";// await Translate(word);
+                    wordsCountList.Add(new WordCount(word, translation, 1));
+                }
+                else
+                {
+                    wordCount.Count++;
+                }
+            }
+
+            return GetResponse(wordsCountList.OrderByDescending(w => w.Count));
+
+            //return wordsCountList.OrderByDescending(w => w.Count);
+        }
+
+        public async Task<string> Translate(string word)
+        {
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={sourceLang}&tl={targetLang}&dt=t&q={word}";
+            var response = await client.GetAsync(url);
+            
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var translation = await response.Content.ReadAsStringAsync();
+                translation = translation.Substring(translation.IndexOf("\""));
+                translation = translation.Substring(0, translation.IndexOf("\""));
+
+                return translation;
+            }
+            return string.Empty;
+        }
+
+        public string GetResponse(IEnumerable<WordCount> words)
+        {
+            var result = "";
+            foreach(var word in words)
+            {
+                result += $"{word.Word},{word.Count}\n";
+            }
+            return result;
+        }
+    }    
+}
+
+//https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText)
+//https://translate.googleapis.com/translate_a/single?client=gtx&sl=pt&tl=en&dt=t&q=Olá
---
+++ Api/Models/WordCount.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMostUsedWords.Models
+{
+    public class WordCount
+    {
+        public string Word { get; private set; }
+        public string Translation { get; set; }
+        public int Count { get; set; }
+
+        public WordCount(string word, string translation, int count)
+        {
+            Word = word;
+            Translation = translation;
+            Count = count;
+        }
+    }
+}
