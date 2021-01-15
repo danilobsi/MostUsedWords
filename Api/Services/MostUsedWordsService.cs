9b5d9a876c5ac5e0e8e4bbbcae0e4e85560b2a5d
Danilo Batista da Silva <dsilva@travix.com>
2021-01-15 17:18:26 +0100

    Removes comment

--- Api/Services/MostUsedWordsService.cs
+++ Api/Services/MostUsedWordsService.cs
@@ -18,7 +18,7 @@ namespace MyMostUsedWords.Services
 
         public IEnumerable<KeyValuePair<string, WordCount>> Get(StreamReader textReader, string sourceLang = "en", string targetLang = "nl")
         {
-            var wordsCountList = new Dictionary<string, WordCount>();
+            var wordsDictionary = new Dictionary<string, WordCount>();
 
             using (var word = new ArrayBuffer<char>(200))
             {
@@ -27,39 +27,19 @@ namespace MyMostUsedWords.Services
                     var ch = textReader.Read();
 
                     if (IsEndOfWordCharacter(ch))
-                        AddWordToDictionary(wordsCountList, word, sourceLang, targetLang);
+                        AddWordToDictionary(wordsDictionary, word, sourceLang, targetLang);
                     else
                         word.Add((char)ch);
                 }
-                AddWordToDictionary(wordsCountList, word, sourceLang, targetLang);
+                AddWordToDictionary(wordsDictionary, word, sourceLang, targetLang);
             }
 
-            return wordsCountList.OrderByDescending(w => w.Value.Count);
+            return wordsDictionary.OrderByDescending(w => w.Value.Count);
         }
 
         private static bool IsEndOfWordCharacter(int ch)
         {
-            //return ch >= 32 && ch <= 47;//65 and 122 - alphabet
             return ch > 122 || ch < 65;
-        }
-
-        private void AddWordToDictionary(Dictionary<string, WordCount> wordsCountList, ArrayBuffer<char> wordBuffer, string sourceLang, string targetLang)
-        {
-            var word = new string(wordBuffer.ToArray());
-            word = word.ToLower();
-
-            if (string.IsNullOrWhiteSpace(word))
-                return;
-
-            if (wordsCountList.ContainsKey(word))
-                wordsCountList[word].Increment();
-            else
-            {
-                var translation = _translatorService.Translate(word, sourceLang, targetLang).Result;
-                wordsCountList.Add(word, new WordCount(word, translation));
-            }
-
-            wordBuffer.Clear();
         }
     }
 }
